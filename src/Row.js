8801c277b5a104089d21463750e155ee03e1c905
jayEom <djawjdgus93@naver.com>
2021-09-26 14:20:54 +0900

    #0-5 : Trailer

--- src/Row.js
+++ src/Row.js
@@ -1,4 +1,7 @@
 import React, { useEffect, useState } from 'react'
+import YouTube from 'react-youtube';
+
+import movieTrailer from 'movie-trailer'
 
 import './Row.css'
 import axios from './axios';
@@ -7,6 +10,7 @@ const base_url = 'https://image.tmdb.org/t/p/original/';
 
 function Row({ title, fetchUrl, isLargeRow }) {
     const [movies, setMovies] = useState([]);
+    const [trailerUrl, setTrailerUrl] = useState('');
 
     useEffect(() => {
         async function fetchData() {
@@ -16,6 +20,27 @@ function Row({ title, fetchUrl, isLargeRow }) {
 
         fetchData();
     }, [fetchUrl])
+    
+    const opts = {
+        height: '390',
+        width: '100%',
+        playerVars: {
+            autoplay: 1
+        }
+    }
+
+    const handleClick = (movie) => {
+        if(trailerUrl) {
+            setTrailerUrl('')
+        } else {
+            movieTrailer(movie?.name || '')
+                .then((url) => {
+                    const urlParams = new URLSearchParams(new URL(url).search);
+                    setTrailerUrl(urlParams.get('v'));
+                })
+                .catch((error) => console.log(error))
+        }
+    }
 
     return (
         <div className='row'>
@@ -27,9 +52,18 @@ function Row({ title, fetchUrl, isLargeRow }) {
                         key={movie.id}
                         className={`row__poster ${isLargeRow && 'row__posterLarge'}`}
                         src={`${base_url}${isLargeRow ? movie.poster_path : movie.backdrop_path}`} 
-                        alt={movie.name} />
+                        alt={movie.name} 
+                        onClick={() => handleClick(movie)}
+                    />
                 ))}
             </div>
+            
+            {trailerUrl &&
+                <YouTube 
+                    videoId={trailerUrl}
+                    opts={opts}
+                />
+            }
         </div>
     )
 }
